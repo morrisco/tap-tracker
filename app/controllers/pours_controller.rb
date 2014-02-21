2ff95b1118335d65c3476415571a143b6e261d5a
Max Morris-Cohen <max.morris-cohen@centro.net>
2014-02-21 16:16:23 -0600

    basic models and relationships

--- app/controllers/pours_controller.rb
+++ app/controllers/pours_controller.rb
@@ -16,6 +16,6 @@ class PoursController < ApplicationController
   private
 
   def pour_params 
-    params.require(:pour).permit(:volume)
+    params.require(:pour).permit(:volume, :keg_id, :user_id)
   end
 end
