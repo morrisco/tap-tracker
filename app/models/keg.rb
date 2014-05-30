24c64314696b1ece418529c91a281d2280b0347f
Max Morris-Cohen <max.morris-cohen@centro.net>
2014-05-30 14:49:29 -0500

    implemented volume decrease with pours

--- app/models/keg.rb
+++ app/models/keg.rb
@@ -1,11 +1,25 @@
 class Keg < ActiveRecord::Base
   has_many :pours
+  validates :name, presence: true
+  after_initialize :init
+
+  def init
+    self.volume ||= 0.0
+  end
 
   def self.current_kegs
-    where(currently_tapped: true).pluck(:name)
+    where(currently_tapped: true)
   end 
 
   def volume_in_gallons
     (volume || 0) / 128
+  end
+
+  def volume_remaining
+    volume - total_poured
+  end
+
+  def total_poured
+    pours.map(&:volume).reduce(0, :+)
   end
 end
